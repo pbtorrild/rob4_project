ea3d2e3a05de118923260d73449a7be6fedfaa47
pbtorrild <birktorrild@me.com>
2019-04-13 23:21:50 +0200

    v0.1

---
+++ src/test.cpp
@@ -0,0 +1,49 @@
+#include <ros/ros.h>
+#include <rob4_project/ClosestObj.h>
+#include <geometry_msgs/PoseStamped.h>
+class movement{
+  public:
+    //Class constructor
+    movement(const ros::Publisher& pub){
+      ros::Publisher pub_=pub; //we define the publicher as the publisher send with the class defenition
+    };
+
+    //Consrtuct other functions:
+    void callback_closest_obj(const rob4_project::ClosestObj::ConstPtr& reseved_data){
+      float dist2obj=reseved_data->distance;
+      float dist_th=0.4;
+
+      geometry_msgs::Twist send_data;
+      send_data.linear.x=1;
+
+      ros::Rate loop_rate(100);
+      do {
+        pub_.publish(send_data);
+        ros::spinOnce();
+        loop_rate.sleep();
+      } while(dist2obj>dist_th);
+
+      send_data.linear.x=0;
+      pub_.publish(send_data);
+    }
+   private:
+     ros::Publisher pub_;
+
+     double x_pos;
+     double y_pos;
+     double z_pos;
+     double w_pos;
+};
+int main(int argc, char**argv){
+  //standart ros initialize
+  ros::init(argc, argv, "movement_pub");
+  ros::NodeHandle nh;
+  //define publisher
+  ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("move_base_simple/goal", 100);
+  //create class instance monitor
+  movement monitor(pub);
+  //subscribe to the topic closest_object with que size 100 and
+  //go to fucktion callback_closest_obj in the class movement with instance monitor
+  ros::Subscriber sub = nh.subscribe("closest_object",100,&movement::callback_closest_obj,&monitor);
+  ros::spin();
+}
