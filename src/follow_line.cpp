4f209dcdecc128d26e91fd222be4a2ebf1961c58
pbtorrild <birktorrild@me.com>
2019-05-06 21:56:44 +0200

    Major fixes by filip and Jasper

--- src/follow_line.cpp
+++ src/follow_line.cpp
@@ -9,95 +9,87 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <cv_bridge/cv_bridge.h>
 
-void lines(cv::Mat im) {
-  cv::Mat input=im;
-  cv::Mat frame=im;
-  cv::Mat mask;
-  int avg_Xs, avg_Ys, avg_Xe, avg_Ye, delta_D;
-  double slope, constant, D_desired = 0, D, sum_D, avg_D;
-	cv::Mat threshold1;
+cv::Mat input, frame, threshold1;
+
+
+double Lines(cv::Mat& im) {
+
+
+
+	//converts it to grayscale
 	cv::cvtColor(im, threshold1, cv::COLOR_BGR2GRAY);
-	cv::cvtColor(im, mask, cv::COLOR_BGR2HSV);
-	cv::inRange(mask, cv::Scalar(0, 0, 0), cv::Scalar(180, 50, 50), mask);
 
-	cv::Mat element1 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(19, 19));
-	cv::morphologyEx(mask, mask, cv::MORPH_CLOSE, element1);
+	//thresholds out only black and dark gray colors, 70 is current treshold,  set it lower and it will detect only darker black
+	cv::threshold(threshold1, threshold1, 70, 255, cv::THRESH_BINARY_INV);
 
-	//Canny(mask, mask, 75, 150);
-	std::vector<cv::Vec4i> lines;
-	cv::HoughLinesP(mask, lines, 1, CV_PI / 180, 50, (input.rows / 3) - 30, 5);
+	//Converts image format back to BRG, otherwise other functions dont work
+	cv::cvtColor(threshold1, threshold1, cv::COLOR_GRAY2BGR);
 
-	int sum_Xs = 0, sum_Ys = 0, sum_Xe = 0, sum_Ye = 0;
-	for (size_t i = 0; i < lines.size(); i++) {
+	//some variables to do some maths
 
-		cv::Vec4i l = lines[i];
-		if (l[1] < frame.rows / 2) {
-			sum_Xs = sum_Xs + l[0];
-			sum_Ys = sum_Ys + l[1];
-			sum_Xe = sum_Xe + l[2];
-			sum_Ye = sum_Ye + l[3];
-		}
-		else {
-			sum_Xs = sum_Xs + l[2];
-			sum_Ys = sum_Ys + l[3];
-			sum_Xe = sum_Xe + l[0];
-			sum_Ye = sum_Ye + l[1];
-		}
-		cv::circle(input, cv::Point(l[0], l[1]), 3, cv::Scalar(0, 0, 255), -1);
-		cv::circle(input, cv::Point(l[2], l[3]), 3, cv::Scalar(255, 0, 0), -1);
+	float sumx = 0;
+	int g = 0;
+	double trackx;
+	int yvalue = 0;
+
+	//checks top row on cutout picture from original full framme, to check different row on cutout picture, change the "yvalue" line up
+	//checks if the line is present on each pixel from the thresholded picture, if yes adds the distance from left corner to that pixel
+
+	for (int x = 0; x < im.cols &&ros::ok(); x++) {
+
+		if(threshold1.at<cv::Vec3b>(cv::Point(x, yvalue))[0] > 1)
+			{
+    		sumx = sumx + x;
+    		g = g+1;
+		  }
 	}
-
-	if (lines.size() > 0) {
-		avg_Xs = (sum_Xs / lines.size());
-		avg_Ys = (sum_Ys / lines.size());
-		avg_Xe = (sum_Xe / lines.size());
-		avg_Ye = (sum_Ye / lines.size());
-	}
-
-	if (avg_Xe - avg_Xs != 0) {
-		slope = (avg_Ye - avg_Ys) / (avg_Xe - avg_Xs);
-		constant = avg_Ys - slope * avg_Xs;
-	}
-
-	cv::line(input, cv::Point((1 - constant) / slope, 1), cv::Point((input.rows - constant) / slope, input.rows), cv::Scalar(255, 0, 255), 4, 0);
-	cv::line(input, cv::Point(avg_Xs, avg_Ys), cv::Point(avg_Xe, avg_Ye), cv::Scalar(0, 255, 0), 4, 0);
-
-	cv::Point fre = cv::Point(((((input.rows * 2) / 3) - constant) / slope) , (input.rows * 2) / 3);
-	circle(input, fre, 10, cv::Scalar(255, 0, 0), -1);
-	cv::circle(input, cv::Point(avg_Xs, avg_Ys), 5, cv::Scalar(0, 0, 0), -1);
-	cv::circle(input, cv::Point(avg_Xe, avg_Ye), 5, cv::Scalar(0, 0, 0), -1);
-
-	D = ((((input.rows * 2) / 3) - constant) / slope)- input.cols / 2;
-	delta_D = D - D_desired ;
-
-
-  if (delta_D>=-input.rows&&delta_D<=input.rows) {
-    //ros::Publisher
-  	ROS_INFO("distance:%d ",delta_D);
-  }
-	cv::Point TrackPoint = cv::Point(input.cols / 2 + D_desired, (input.rows * 2) / 3);
-	cv::circle(input, TrackPoint, 10, cv::Scalar(0, 128, 255), -1);
-
-  cv::imshow("input",input);
-
+	//finds average distance to those pixels, a.k.a middle of the line
+	trackx = sumx / g;
+	//returns distance, again the one from the cutout picture
+	return trackx;
 }
+
 
 void imageCallback(const sensor_msgs::ImageConstPtr msg)
 {
+  cv::Mat threshold1;
   cv_bridge::CvImagePtr cv_ptr;
   try
   {
     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-    cv::waitKey(30);
   }
   catch (cv_bridge::Exception& e)
   {
     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
   }
   //image pros
-  cv::Mat frame = cv_ptr->image;
-  lines(frame);
+  input = cv_ptr->image;
+  // here you select the area you want to chceck for finding the line, be carefull, when selecting the entire image,
+  //then it will check only the top row later in algorythm,
+  //to change that change the y-value, unit vector of y points down from top left corner btw
+	cv::Rect inputRect = cv::Rect((input.cols * 2) / 3, (input.rows * 2) / 3, input.cols / 3, input.rows / 3);
+
+	//cuts out the desired area
+	cv::Mat frame = input(inputRect);
+	// runs the algorythm as well as calculating the distance from left corner of the original non cut frame
+	double distance = Lines(frame) + input.cols/3*2;
+
+	// slaps a point on the original image so you can see if it works
+  //show referal points
+  cv::Point refereal_point = cv::Point(distance, input.rows*2/3);
+	cv::circle(input, refereal_point, 8, cv::Scalar(255, 255, 255), -1);
+
+	cv::Point TrackPoint1 = cv::Point(distance, input.rows*2/3);
+	cv::circle(input, TrackPoint1, 8, cv::Scalar(0, 0, 255), -1);
+
+  // shows the selected area for finding the line
+  cv::rectangle(input, inputRect, cv::Scalar(255, 255, 255), 2, 8, 0);
+
+
+	 //shows the original img with the dot on top of the line hopefully
+	cv::imshow("view", input);
+  cv::waitKey(30);
+  ROS_INFO("%f",distance);
 }
 
 int main(int argc, char **argv)
--- src/test.cpp
+++ src/test.cpp
@@ -8,8 +8,11 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <cv_bridge/cv_bridge.h>
 
-// creates Matrix src andd frame, used in the imageCallback function
-cv::Mat frame,src;
+//Puts input image input into "video"                   Path to video here
+cv::VideoCapture video = cv::VideoCapture("/home/peter/catkin_ws/src/torrilds_package/src/templates/videotest.mp4");
+// creates Matrix src andd frame, used in the main function
+cv::Mat src, frame;
+
 //setabel funktion, takes image to print label on, the string to print, and object index as input
 //The entire funktion is a placeholder for the output the TurtleBot needs to act on the sign given
 void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& contour){
@@ -61,54 +64,38 @@ int minArr(double arr[], int n) {
 //an array used to find the best fitting template, the colour index and the shape index added together, the object index number, and the array containing the template indexes
 //phuu... that was a long one
 int match(cv::Mat templateArray[], double ITS[], int images, cv::Rect sqr1, double bestFit[], int colourshape, std::vector<cv::Point>& contour1, int sign[]) {
-	ROS_INFO("match found");
+
 	//A for loop that will runs once for each template
 	for (int i = 0; i < images; i++) {
-		ROS_INFO("m1");
 		//Creating a matrix the same size as the current template
 		cv::Mat Temp_1R = cv::Mat(templateArray[i].size(), CV_8U);
-		ROS_INFO("m2");
 		//Creating a matrix the same size as the bounding box
 		cv::Mat crop = cv::Mat(sqr1.size(), CV_8U);
-		ROS_INFO("m3");
 		//Puts whatevver is inside the bounding box in the inout image into the matrix "crop"
 		crop = frame(sqr1);
-		int cols = crop.cols;
-		int rows =crop.rows;
-
-		ROS_INFO("m4 ,%d, %d",cols,rows);
 		//Resizes the template to the detected sign to get the best match, and puts the image in the Temp_1R matrix
-		cv::resize(templateArray[i], Temp_1R, cv::Size(cols*ITS[i], rows*ITS[i]));
-		ROS_INFO("m5");
+		resize(templateArray[i], Temp_1R, cv::Size(sqr1.width*ITS[i], sqr1.height*ITS[i]));
+
 		//Converts the template matrix to greyscale
 		cvtColor(Temp_1R, Temp_1R, cv::COLOR_BGR2GRAY);
-		ROS_INFO("m6");
 		//Converts the image to greyscale
 		cvtColor(crop, crop, cv::COLOR_BGR2GRAY);
-		ROS_INFO("m7");
 		//Does a threshold for the cropped image to make it binary
 		threshold(crop, crop, 80, 255, cv::THRESH_BINARY_INV);
-		ROS_INFO("m8");
 
 		//Creates the output matrix for the mathing funtion
 		cv::Mat result_1;
-		ROS_INFO("m9");
 		//Check how well the second input mathes the first input
 		matchTemplate(crop, Temp_1R, result_1, cv::TM_SQDIFF);
-		ROS_INFO("m10");
 		//Used to store the best and worst match of the single template in the given bounding box
 		double minVal, maxVal;
-		ROS_INFO("m11");
 		//Used to determin where on the image minVal and maxVal is located
 		cv::Point minLoc, maxLoc;
-		ROS_INFO("m12");
 		//Saves the values to the corrosponding int's
 		minMaxLoc(result_1, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat());
-		ROS_INFO("m13");
 
 		//Saves the minVal in an array initialised in the funtion inside
 		bestFit[i] = minVal;
-		ROS_INFO("m done :()");
 	}
 	//Calls for the funtion minArr and put the result into fitIndex
 	int fitIndex = minArr(bestFit, images);
@@ -118,32 +105,27 @@ int match(cv::Mat templateArray[], double ITS[], int images, cv::Rect sqr1, doub
 
 //inside funtion, takes bounding box, the shape-type index, colour + shape-type, and the contour index as input
 void inside(cv::Rect sqr, int shapetype, int colourshape, std::vector<cv::Point>& contour){
-	ROS_INFO("i break now 1");
+
 	//Creates an array containing the index number of all the signs recognizable
 	int signIndex[] = { 111, 112, 113, 131, 132, 221 };
 	//corresponding string names for each sign
-	ROS_INFO("i break now 2");
 	std::string signLabel[] = { "MainSideRoad", "Yield", "Kids", "DontGoLeft", "70", "Cross" };
 	//Creates an int called sign, this is not used right here, but will be later on
 	int sign;
 
 	// if statement checking what kind of sign we are dealing with
 	if (shapetype == 10) {
-		ROS_INFO("i break now A.3");
+
 		//Array containing the paths to all the templates, it is important they are in order
-		cv::Mat triangles[] = { cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/mainsideroad"), cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/yield"), cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/kids ")};
+		cv::Mat triangles[] = { cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/mainsideroad.png"), cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/yield.png"), cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/kids.png")};
 		//Array containg the ratio between the sign and the image insige it, this is used to scale the template later
-		ROS_INFO("i break now A.4");
 		double triITSratio[] = {0.345368, 0.516129032, 0.3577405858};
 		//Index number of each template in an array
-		ROS_INFO("i break now A.5");
 		int triSign[] = {1, 2, 3};
 		//An array with as many elements as templates for the specific shape
-		ROS_INFO("i break now A.6");
 		double bestFitTri[3] = {};
 
 		// this int is used to count number of templates
-		ROS_INFO("i break now A.3");
 		int triimages = 0;
 		//for loop that runs as many times as the number of templates
 		for (cv::Mat element : triangles) {
@@ -158,7 +140,7 @@ void inside(cv::Rect sqr, int shapetype, int colourshape, std::vector<cv::Point>
 	if(shapetype == 20){
 
 		//Array containing the paths to all the templates, it is important they are in order
-		cv::Mat rectangles[] = { cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/WalkerTemp ")};
+		cv::Mat rectangles[] = { cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/WalkerTemp.png")};
 		//Array containg the ratio between the sign and the image insige it, this is used to scale the template later
 		double rectITSratio[] = {0.627659};
 		//Index number of each template in an array
@@ -179,19 +161,16 @@ void inside(cv::Rect sqr, int shapetype, int colourshape, std::vector<cv::Point>
 
 	// if statement checking what kind of sign we are dealing with
 	if (shapetype == 30) {
-		ROS_INFO("i break now C.3");
+
 		//Array containing the paths to all the templates, it is important they are in order
-		cv::Mat circles[] = { cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/dontgoleft "), cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/70 ") };
+		cv::Mat circles[] = { cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/dontgoleft.png"), cv::imread("/home/peter/catkin_ws/src/torrilds_package/src/templates/70.png") };
 		//Array containg the ratio between the sign and the image insige it, this is used to scale the template later
-		ROS_INFO("i break now C.4");
 		double cirITSratio[] = { 0.5091743, 0.5019762 };
 		//Index number of each template in an array
-		ROS_INFO("i break now C.5");
 		int cirSign[] = { 1, 2 };
 		//An array with as many elements as templates for the specific shape
-		ROS_INFO("i break now C.6");
 		double bestFitCir[2] = {};
-		ROS_INFO("i break now C.7");
+
 		// this int is used to count number of templates
 		int cirimages = 0;
 		//for loop that runs as many times as the number of templates
@@ -199,9 +178,8 @@ void inside(cv::Rect sqr, int shapetype, int colourshape, std::vector<cv::Point>
 			//Will end up to be equal to the number of templates
 			cirimages++;
 		}
-		ROS_INFO("i break now C.8 :()");
 		//Here the int sign is used and set equal to the out put of the function match, wich is also called. Inputs are explained at the funtion
-		sign = match(circles, cirITSratio, 2, sqr, bestFitCir, colourshape, contour, cirSign);
+		sign = match(circles, cirITSratio, cirimages, sqr, bestFitCir, colourshape, contour, cirSign);
 	}
 
 	// this int is used to count number of signs detectable
@@ -225,17 +203,16 @@ void inside(cv::Rect sqr, int shapetype, int colourshape, std::vector<cv::Point>
 		}
 	}
 	//If j is smaller tempimages (number of elements in signLabel (array containing the string names of detectable signs))
-	if (j < tempimages)
+	if (j < tempimages) {
 		//Calls for setLabel with the image input, the name of the detected sign, and the specific object index number as input
 		//The entire funktion is a placeholder for the output the TurtleBot needs to act on the sign given
-		//setLabel(src, signLabel[j], contour);
-		ROS_INFO("Sign:%d",sign);
-	//}
+		setLabel(src, signLabel[j], contour);
+	}
 }
 
 //findShapes funtion. Takes thresholded image and a colour index as input
 void findShapes(cv::Mat& im, int colour) {
-	ROS_INFO("Shape found");
+
 	//Creating morphing elements
 	cv::Mat element1 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(19, 19));
 	cv::Mat element2 = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
@@ -248,7 +225,7 @@ void findShapes(cv::Mat& im, int colour) {
 	imshow("Canny", im);
 
 	//IDFK, something somthing contours...
-	std::vector<std::vector<cv::Point> > contours;
+	std::vector<std::vector<cv::Point>> contours;
 	// Finds contours... who would have guessed...
 	findContours(im, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_NONE);
 	//Approximates the vertices (corners)
@@ -305,15 +282,14 @@ void findShapes(cv::Mat& im, int colour) {
 			}
 		}
 	}
-
 }
 
 // findColour funtion. Takes the ROI as input
 void findColour(cv::Mat& im) {
-	ROS_INFO("Color found");
+
 	//Creates a matrix for each colour to look for, with the same amount of colums and rows as the inout image
 	cv::Mat red = cv::Mat(im.rows, im.cols, CV_8U);
-	//cv::Mat blue = cv::Mat(im.rows, im.cols, CV_8U);
+	cv::Mat blue = cv::Mat(im.rows, im.cols, CV_8U);
 
 	/*
 		When usning the inRange funtion, it takes an HSV image (Hue, Saturation, Value) input.
@@ -334,7 +310,7 @@ void findColour(cv::Mat& im) {
 	// converting from RGB to HSV
 	cvtColor(im, red, cv::COLOR_RGB2HSV);
 	// converting from BGR to HSV
-	//cvtColor(im, blue, cv::COLOR_BGR2HSV);
+	// cvtColor(im, blue, cv::COLOR_BGR2HSV);
 	// Using the inRange funtion to threshold a specific colour
 	inRange(red, cv::Scalar(100, s1, v1), cv::Scalar(140, s2, v2), red);
 	//inRange(blue, cv::Scalar(100, s1, v1), cv::Scalar(140, s2, v2), blue);
@@ -343,7 +319,6 @@ void findColour(cv::Mat& im) {
 	//findShapes(blue, 200);
 
 }
-
 void imageCallback(const sensor_msgs::ImageConstPtr msg)
 {
   cv_bridge::CvImagePtr cv_ptr;
