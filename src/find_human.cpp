2682222d5e7921655e30e0f9d0060123bd2ed7ce
pbtorrild <birktorrild@me.com>
2019-05-15 14:59:32 +0200

    find humas space too much

--- src/find_human.cpp
+++ src/find_human.cpp
@@ -5,7 +5,6 @@
 #include "sensor_msgs/image_encodings.h"
 #include <image_transport/image_transport.h>
 
-
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
