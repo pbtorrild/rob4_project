1f444d5f0e37c9db3e5cc3605a2af5be6d2fb65b
pbtorrild <birktorrild@me.com>
2019-05-17 12:59:21 +0200

    control v 0.1

--- src/control.cpp
+++ src/control.cpp
@@ -11,22 +11,28 @@ private:
   //distance in pixel to line
   float desired_pix_dist=320;
   // vel
-  float std_vel =0.05;
-  float speed_up_vel=0.3;
+  float vel_30=0.01;
+  float std_vel =0.03;
+  float vel_50 = 0.03;
+  float vel_70 =0.05;
+  float speed_up_vel=0.05;
 protected:
   //emergcy stop status
   bool emerg_stop=true;
   bool emerg_speed_up=false;
   //sign input
-  bool MainSideRoad;
-  bool Yield;
-  bool Kids;
-  bool DontGoLeft;
-  bool Seventy;
-  bool Thirty;
-  bool Fifty;
-  bool BothWaysNo;
-  bool Cross;
+  bool MainSideRoad=false;
+  bool Yield=false;
+  bool Kids=false;
+  bool DontGoLeft=false;
+  bool Seventy=false;
+  bool Thirty=false;
+  bool Fifty=false;
+  bool BothWaysNo=false;
+  bool Cross=false;
+  //humans and car detection
+  bool human=false;
+  bool car=false;
   //change of road
   float line_dist_px=0;
 public:
@@ -37,7 +43,7 @@ public:
     double dist_2l_error=distance_in-desired_pix_dist;
     ROS_INFO("Error: %f",dist_2l_error);
       //we determine k by taking the maximum wanted angular vel and diviting it by the maximum error
-      float k=0.0015; //A_vel_max/e_max , A_vel_max=2 & e_max=320
+      float k=0.0015; //A_vel_max/e_max , A_vel_max=0.5 & e_max=320
       angular_vel=-dist_2l_error*k;
     return angular_vel;
   }
@@ -47,11 +53,31 @@ public:
     //control funcktions
     //fist check for emergency stop
     if (emerg_stop!=true) {
-      //sign input
-      send_data.linear.x=std_vel;
+      send_data.linear.x=std_vel; // go with a std vel of 50 km/h
+
+      //sign to determine speed
+      if (Seventy=true) {
+        send_data.linear.x=vel_70;
+      }
+      if (Fifty=true) {
+        send_data.linear.x=vel_50;
+      }
+      if (emerg_speed_up==true) {
+        send_data.linear.x=speed_up_vel;
+      }
       //////////////////////////////
-      //Do the angular change
+      //Do the standard angular change
       send_data.angular.z=get_angular_vel(line_dist_px);
+
+      //////////////////////////////
+      // Special cases of change in vel and angle
+
+      //perfon a stop if there is a human at a crossing
+      if (human=true && Cross=true) {
+        send_data.linear.x=0;
+        send_data.angular.z=get_angular_vel(line_dist_px);
+      }
+
     }
     else{
       send_data.linear.x=0;
@@ -100,8 +126,8 @@ int main(int argc, char**argv){
   control_data monitor;
   //subscibers
   ros::Subscriber sub_signs = nh.subscribe("signs_found",1,&control_data::callback_signs_found,&monitor);
-  ros::Subscriber sub_emerg_stop = nh.subscribe("emerg_stop_status",100,&control_data::callback_emerg_stop,&monitor);
-  ros::Subscriber sub_road_change = nh.subscribe("line_dist",100,&control_data::callback_road_change,&monitor);
+  ros::Subscriber sub_emerg_stop = nh.subscribe("emerg_stop_status",1,&control_data::callback_emerg_stop,&monitor);
+  ros::Subscriber sub_road_change = nh.subscribe("line_dist",1,&control_data::callback_road_change,&monitor);
   //initialise the node with an emerg_stop
   ros::Rate rate(15.);
   while (ros::ok()) {
